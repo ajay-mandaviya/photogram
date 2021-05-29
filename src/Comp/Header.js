46dc669f7152b0492cb985ba73455adcd2e3a72c
AJAY12398 <ajay.mandaviya03@gmail.com>
2021-05-29 15:58:34 +0530

    add auth

--- src/Comp/Header.js
+++ src/Comp/Header.js
@@ -1,23 +1,47 @@
-import React from 'react'
-import './Header.css'
-const Header = () =>{
+import React from "react";
+import "./Header.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { projectAuth } from "../firebase/config";
+const Header = ({ setAuthenticate, description, click, setClick , setShowError }) => {
+  const [user] = useAuthState(projectAuth);
+  // const handleSingIn = () =>{
+  // console.log("click")
+  // !user ? projectAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider()) : projectAuth.signOut();
+  // }
 
+  const handleSingup = () => {
+    // console.log("click")
+    user ? projectAuth.signOut() : setAuthenticate(true);
+    setClick(false);
+    //show signup box make val true
+  };
 
-    return (
-        <div >
-        <div className  = "header">
+  return (
+    <div>
+      <div className="header">
         <h2>PhotoGram</h2>
-        <div className   = "btn">
-            <button>Sign in</button>
-            <button>Sign up</button>
+        <div className="btn">
+          <button
+            onClick={() => {
+              setClick(!click);
+              !user &&   setShowError(true);
+            }}
+          >
+            {!click ? "Your post" : "Public"}
+          </button>
+          <button
+            onClick={handleSingup} //lets show popup box
+          >
+            {user ? "Sign Out" : "Sign In"}
+          </button>
         </div>
-        </div>
-        <hr></hr>
-        <div className = "title">
-            <h2>Your Picture</h2>
-            <p>Upload Your Best Picture and Share With Your Friends</p>
-        </div>
-        </div>
-    )
-}
-export default Header
+      </div>
+      <hr></hr>
+      <div className="title">
+        <h2>{click ? "Your Post" : "Public Post"}</h2>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+export default Header;
